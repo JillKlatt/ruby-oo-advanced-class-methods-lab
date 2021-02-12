f6d63efd5778991cc58b227055a326414cdd670e
JillKlatt <klatt.jill@gmail.com>
2021-02-12 10:38:58 -0500

    commit tree b06ac89c74b727899e54f7a3ee25312c5200cea5

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,72 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+  song = self.all.detect {|song| song.name == title}
+  song
+  end
+
+  def self.find_or_create_by_name(title)
+    song = self.find_by_name(title)
+    if song
+      song
+    else
+      self.create_by_name(title)
+    end
+  end
+
+  def self.alphabetical
+    songs = self.all.sort_by {|song| song.name}
+    songs
+  end
+
+  def self.new_from_filename(input)
+    answer = input.split(" - ")
+    answer[1] = answer[1].chomp(".mp3")
+    song = self.new
+    song.artist_name = answer[0]
+    song.name = answer[1]
+    song
+  end
+
+  def self.create_from_filename(input)
+    answer = input.split(" - ")
+    answer[1] = answer[1].chomp(".mp3")
+    song = self.create
+    song.artist_name = answer[0]
+    song.name = answer[1]
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
+
+
+# binding.pry
+
+# answer[0] = artist
+# answer[1..-1] = song_name
+# return "#{"artist"}" - "#{song_name}"
+#makes answer look like #"Artist - Name"
+#split artist (first word) from array
+
